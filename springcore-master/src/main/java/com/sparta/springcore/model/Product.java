414ce88312770fceecaa735c9a2c137673316288
jjems <jjems102@gmail.com>
2022-08-01 19:05:31 +0900

    최저가까지

--- springcore-master/src/main/java/com/sparta/springcore/model/Product.java
+++ springcore-master/src/main/java/com/sparta/springcore/model/Product.java
@@ -11,7 +11,7 @@ import javax.persistence.*;
 @Getter // get 함수를 일괄적으로 만들어줍니다.
 @NoArgsConstructor // 기본 생성자를 만들어줍니다.
 @Entity // DB 테이블 역할을 합니다.
-public class Product extends Timestamped{
+public class Product {
 
     // ID가 자동으로 생성 및 증가합니다.
     @GeneratedValue(strategy = GenerationType.AUTO)
@@ -47,5 +47,4 @@ public class Product extends Timestamped{
         this.lprice = requestDto.getLprice();
         this.myprice = 0;
     }
-
 }
